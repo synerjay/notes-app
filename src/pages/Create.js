9b3e7f1a4efcff6164754aa902e0db492161f8ce
Jerome Tolentino <tolent.jay@gmail.com>
2021-03-20 16:17:25 +0900

    Added buttons

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,8 +1,33 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
-
 // OR "import { Typography } from 'material-ui/core/' "
 // import Material UI in two ways
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import { Container } from '@material-ui/core';
+import KeyboardArrowRightOutlinedIcon from '@material-ui/icons/KeyboardArrowRightOutlined';
+
 export default function Create() {
-  return <div>Create page</div>;
+  return (
+    <Container>
+      <Typography
+        variant='h6'
+        color='textSecondary'
+        component='h2'
+        gutterBottom
+      >
+        Create a New Note
+      </Typography>
+
+      <Button
+        onClick={() => console.log('You Clicked ME!!!!')}
+        type='submit'
+        color='secondary'
+        variant='contained'
+        endIcon={<KeyboardArrowRightOutlinedIcon />}
+      >
+        Submit
+      </Button>
+    </Container>
+  );
 }
