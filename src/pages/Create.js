a72ed298936ebb8c01ad8e50deeca40eb6194451
Jerome Tolentino <tolent.jay@gmail.com>
2021-03-20 18:34:49 +0900

    Added Text field checkpoints and error messages

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Typography from '@material-ui/core/Typography';
 // OR "import { Typography } from 'material-ui/core/' "
 // import Material UI in two ways
@@ -16,6 +16,32 @@ const useStyles = makeStyles({
 
 export default function Create() {
   const classes = useStyles();
+
+  const [title, setTitle] = useState('');
+  const [details, setDetails] = useState('');
+
+  const [titleError, setTitleError] = useState(false);
+  const [detailsError, setDetailsError] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setTitleError(false);
+    setDetailsError(false);
+
+    if (title == '') {
+      setTitleError(true);
+    }
+
+    if (details == '') {
+      setDetailsError(true);
+    }
+
+    if (title && details) {
+      console.log(title, details);
+    }
+  };
+
   return (
     <Container>
       <Typography
@@ -26,25 +52,39 @@ export default function Create() {
       >
         Create a New Note
       </Typography>
-      <form noValidate autoComplete='off'>
+      <form noValidate autoComplete='off' onSubmit={handleSubmit}>
         <TextField
+          onChange={(e) => setTitle(e.target.value)}
           className={classes.field}
           label='Note Title'
           variant='outlined'
           color='secondary'
           fullWidth
           required
+          error={titleError}
         />
+        <TextField
+          onChange={(e) => setDetails(e.target.value)}
+          className={classes.field}
+          label='Details'
+          variant='outlined'
+          color='secondary'
+          multiline
+          rows={4}
+          fullWidth
+          required
+          error={detailsError}
+        />
+        <Button
+          onClick={() => console.log('You Clicked ME!!!!')}
+          type='submit'
+          color='secondary'
+          variant='contained'
+          endIcon={<KeyboardArrowRightOutlinedIcon />}
+        >
+          Submit
+        </Button>
       </form>
-      <Button
-        onClick={() => console.log('You Clicked ME!!!!')}
-        type='submit'
-        color='secondary'
-        variant='contained'
-        endIcon={<KeyboardArrowRightOutlinedIcon />}
-      >
-        Submit
-      </Button>
     </Container>
   );
 }
