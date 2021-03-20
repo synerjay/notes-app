6ab97fc20219c0a946c81c9b985cf451b3c0c035
Jerome Tolentino <tolent.jay@gmail.com>
2021-03-20 15:41:19 +0900

    Initial commit

---
+++ src/pages/Create.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+// OR "import { Typography } from 'material-ui/core/' "
+// import Material UI in two ways
+export default function Create() {
+  return <div>Create page</div>;
+}
