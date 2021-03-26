f9db2a18cdc37d3a3141b90db800cc4feed44302
Jerome Tolentino <tolent.jay@gmail.com>
2021-03-26 17:50:11 +0900

    Added Layout component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Notes from './pages/Notes';
 import Create from './pages/Create';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core';
 import { purple } from '@material-ui/core/colors';
+import Layout from './components/Layout';
 
 // To override the default theme properties, use createMuiTheme and Themeprovider
 const theme = createMuiTheme({
@@ -20,19 +21,21 @@ const theme = createMuiTheme({
     fontWeightBold: 700,
   },
 });
-
+// Layout component need to WRAP the Switch components but NOT the Router component
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <Router>
-        <Switch>
-          <Route exact path='/'>
-            <Notes />
-          </Route>
-          <Route path='/create'>
-            <Create />
-          </Route>
-        </Switch>
+        <Layout>
+          <Switch>
+            <Route exact path='/'>
+              <Notes />
+            </Route>
+            <Route path='/create'>
+              <Create />
+            </Route>
+          </Switch>
+        </Layout>
       </Router>
     </ThemeProvider>
   );
