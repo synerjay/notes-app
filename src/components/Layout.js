e72650e0d57386ac477954ea5b6c6774f4a16be5
Jerome Tolentino <tolent.jay@gmail.com>
2021-03-26 18:16:14 +0900

    Added Permanent Drawer, ready for Links

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,21 @@
-import { makeStyles } from '@material-ui/core';
+import { Drawer, makeStyles, Typography } from '@material-ui/core';
 import React from 'react';
+
+const drawerWidth = 240;
 
 const useStyles = makeStyles({
   page: {
     background: '#f9f9f9',
     width: '100%',
+  },
+  root: {
+    display: 'flex',
+  },
+  drawer: {
+    width: drawerWidth,
+  },
+  drawerPaper: {
+    width: drawerWidth,
   },
 });
 
@@ -12,11 +23,25 @@ const Layout = ({ children }) => {
   const classes = useStyles();
 
   return (
-    <div>
-      {/* app bar */}
+    <div className={classes.root}>
+      {/* App Bar */}
       <div>App bar</div>
-      {/* side bar */}
-      <div>Side bar</div>
+
+      {/* "Permanent" Side Drawer */}
+      <Drawer
+        className={classes.drawer}
+        variant='permanent'
+        classes={{ paper: classes.drawerPaper }}
+        anchor='left'
+      >
+        <div>
+          <Typography variant='h5' className={classes.title}>
+            Ninja Notes
+          </Typography>
+        </div>
+
+        {/* Links/List section */}
+      </Drawer>
       <div className={classes.page}>{children} </div>
     </div>
   );
