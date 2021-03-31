3593540d3c331b759f7b216dff6da4eefb4c37b8
Jerome Tolentino <tolent.jay@gmail.com>
2021-03-31 20:53:28 +0900

    Added masonry component and module

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,6 +1,7 @@
-import { Container, Grid, Paper } from '@material-ui/core';
+import { Container } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
 import NoteCard from '../components/NoteCard';
+import Masonry from 'react-masonry-css';
 
 export default function Notes() {
   const [notes, setNotes] = useState([]);
@@ -19,17 +20,28 @@ export default function Notes() {
     setNotes(newNotes);
   };
 
+  // Breakpoint constant is for Masonry to assign column according to the monitor size, therefore it is responsive
+  const breakpoints = {
+    default: 3, //<-- when its normal size, 3 columns
+    1100: 2, // <-- when its 1100 pixel wide, 2 columns
+    700: 1, //<-- when its 700 or less 1 column
+  };
+
   // The container component creates padding and margins to its child items
   return (
     <Container>
-      <Grid container spacing={3}>
+      <Masonry
+        breakpointCols={breakpoints}
+        className='my-masonry-grid'
+        columnClassName='my-masonry-grid_column'
+      >
         {/* since 12 is the max columns in a row, the small screen gets 12 */}
         {notes.map((note) => (
-          <Grid item xs={12} md={6} lg={4} key={note.id}>
+          <div key={note.id}>
             <NoteCard note={note} handleDelete={handleDelete} />
-          </Grid>
+          </div>
         ))}
-      </Grid>
+      </Masonry>
     </Container>
   );
 }
